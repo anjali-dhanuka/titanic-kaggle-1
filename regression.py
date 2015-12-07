18b10027e7964839d06a5caea832fd1d7beef5d6
karandesai-96 <karandesai_96@live.com>
2015-12-07 09:30:27 +0530

    separate fit and predict by linear and logistic regression models

--- regression.py
+++ regression.py
@@ -1,4 +1,5 @@
-from sklearn.linear_model import LinearRegression
+import pandas as pd
+from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.cross_validation import KFold
 import preprocess_data as preproc
 import numpy as np
@@ -6,13 +7,15 @@ import numpy as np
 predictors = ["Pclass", "Age", "SibSp", "Parch", "Fare", "Embarked"]
 
 lin_reg = LinearRegression()
+log_reg = LogisticRegression(random_state=1)
+
 train_data = preproc.preprocess_silent("train")
 test_data = preproc.preprocess_silent("test")
 
 kfold = KFold(train_data.shape[0], n_folds=3, random_state=1)
 
 
-def hypothesis_fitting():
+def fit_and_predict_linreg():
     predictions = []
     for train, test in kfold:
         train_predictors = train_data[predictors].iloc[train, :]
@@ -29,3 +32,16 @@ def hypothesis_fitting():
 
         accuracy = sum(predictions[predictions == train_data["Survived"]]) / len(predictions)
     return accuracy
+
+
+def fit_and_predict_logreg():
+    log_reg.fit(train_data[predictors], train_data["Survived"])
+
+    predictions = log_reg.predict(test_data[predictors])
+
+    submission = pd.DataFrame({
+        "PassengerId": test_data["PassengerId"],
+        "Survived": predictions
+    })
+
+    return submission
