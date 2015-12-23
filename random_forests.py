ce57290964bbaafb232a2970bf3575f3e80b20ae
karandesai-96 <karandesai_96@live.com>
2015-12-23 22:03:48 +0530

    tunes classifier parameters- n_estimators, min_samples at split and leaf

--- random_forests.py
+++ random_forests.py
@@ -6,13 +6,13 @@ predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
 
 
 def random_forest_impl(train_data):
-    algo = RandomForestClassifier(n_estimators=10, min_samples_split=2,
-                                  min_samples_leaf=1, random_state=1)
+    algo = RandomForestClassifier(n_estimators=150, min_samples_split=4,
+                                  min_samples_leaf=2, random_state=1)
 
     score = cross_validation.cross_val_score(algo, train_data[predictors],
                                              train_data["Survived"], cv=3)
-    
-    print score
+
+    print score.mean()
 
 
 if __name__ == '__main__':
