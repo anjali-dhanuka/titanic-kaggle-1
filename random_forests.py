5494e5002c2f186ed58e9234b171924f279df328
karandesai-96 <karandesai_96@live.com>
2015-12-24 09:39:25 +0530

    Adds a new feature for title of the passenger

--- random_forests.py
+++ random_forests.py
@@ -1,6 +1,7 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import cross_validation
 import preprocess_data as preproc
+import re
 
 predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
 
@@ -20,7 +21,27 @@ def new_features(train_data):
     train_data["NameLength"] = train_data["Name"].apply(lambda x: len(x))
     print "Added two new features: FamilySize and NameLength..."
 
+    titles = train_data["Name"].apply(get_title)
+
+    title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Dr": 5, "Rev": 6, "Major": 7, "Col": 7,
+                     "Mlle": 8, "Mme": 8, "Don": 9, "Lady": 10, "Countess": 10, "Jonkheer": 10, "Sir": 9,
+                     "Capt": 7, "Ms": 2}
+
+    for key, value in title_mapping.items():
+        titles[titles == key] = value
+
+    train_data["Title"] = titles
+    print "Added a new feature: Title..."
+
     return train_data
+
+
+def get_title(name):
+    title_search = re.search(" ([A-Za-z]+)\.", name)
+
+    if title_search:
+        return title_search.group(1)
+    return ""
 
 
 if __name__ == '__main__':
@@ -30,5 +51,5 @@ if __name__ == '__main__':
     titanic_training_data = new_features(titanic_training_data)
     predictors.append("FamilySize")
     predictors.append("NameLength")
-
+    predictors.append("Title")
     random_forest_impl(titanic_training_data)
